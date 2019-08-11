6c8ba786e8ad4864207f4a00ef20172e277187fe
Quang Võ <vnhquang2011@gmail.com>
2019-08-11 23:12:09 +0700

    update

--- Guest-Lookup/index.php
+++ Guest-Lookup/index.php
@@ -20,6 +20,7 @@
 	<link rel="stylesheet" href="css/responsive.css" />
 </head>
 <?php 
+	error_reporting(0);
 	$id = $_GET['id'];
 	$curl = curl_init();
 	curl_setopt_array($curl, array(
@@ -83,7 +84,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="main_banner_area text-center">
-					<div class="col-md-8 col-sm-8 col-md-offset-4 col-sm-offset-4">
+					<div class="col-md-8 col-sm-8 col-md-offset-4 col-sm-offset-5">
 						<div class="single_banner_text wow zoomIn" data-wow-duration="1s">
 							<p>Kính chào quý khách</p>
 							<div class="separetor"></div>
@@ -103,11 +104,15 @@
 		<div class="container">
 			<div class="row">
 				<div class="main_abouts">
-					<div class="col-md-8 col-sm-8 col-xs-12">
+					<div class="col-md-8 col-sm-9 col-xs-12">
 						<div class="single_abouts wow slideInLeft" data-wow-duration="2s">
 							<div class="head_title text-center">
 								<h2 style="color:black;">Tra cứu thông tin</h2>
 								<div class="separetor"></div>
+								<?php 
+									if ($val_arr['unitName'] != NULL) 
+									{
+								?>
 								<div class="info-table">
 									<div class="row-info">
 										<div class="key-info col-sm-4 col-md-4 col-xs-4">Tên sản phẩm</div>
@@ -122,14 +127,19 @@
 										</div>
 									</div>
 									<div class="row-info">
-										<div class="key-info col-sm-4 col-md-4 col-xs-4" >Thuộc lô hàng</div>
+										<div class="key-info col-sm-4 col-md-4 col-xs-4">Tình trạng</div>
 										<div class="val-info col-md-8 col-sm-8 col-xs-8">
-											<?php echo $val_arr['parcelId'] ?>
+											<?php 
+												if ($val_arr['uStatus'] == "SOLD")
+													echo "Sản phẩm đã được bán";
+												else 
+													echo "Đang được bày bán";
+											?>
 										</div>
 									</div>
 									<div class="row-info">
 										<div class="key-info col-sm-4 col-md-4 col-xs-4">Đặc điểm</div>
-										<div class="val-info col-md-8 col-sm-8 col-xs-8">
+										<div class="val-info col-md-8 col-sm-8 col-xs-8 unit-description">
 											<?php echo $val_arr['description'] ?>
 										</div>
 									</div>
@@ -142,11 +152,16 @@
 									<div class="row-info">
 										<div class="key-info col-sm-4 col-md-4 col-xs-4">Ngày bán</div>
 										<div class="val-info col-md-8 col-sm-8 col-xs-8">
-											<?php echo date('d/m/Y H:i:s',strtotime($val_arr['soldDate'])); ?>
+											<?php  
+												if (date('d/m/Y H:i:s',strtotime($val_arr['soldDate'])) < date('d/m/Y H:i:s',strtotime($val_arr['sellingDate'])))
+													echo "";
+												else 
+												 	echo date('d/m/Y H:i:s',strtotime($val_arr['sellingDate']));
+											?>
 										</div>
 									</div>
 									<div class="row-info">
-										<div class="key-info col-sm-4 col-md-4 col-xs-4">Thuộc sở hữu</div>
+										<div class="key-info col-sm-4 col-md-4 col-xs-4"><?php if($val_arr['uStatus'] == "SELLING") echo "Đang được bán tại"; else echo "Nơi bán"; ?></div>
 										<div class="val-info col-md-8 col-sm-8 col-xs-8">
 											<?php 
 												$ownerid = explode('#',$val_arr['owner'],2)[1];
@@ -165,11 +180,20 @@
 										</div>
 									</div>
 								</div>
+								<?php 
+									}
+									else
+									{
+								?>
+									<div class="info-table" style="text-alige:center;"> <Span>Không tìm thấy sản phẩm! <br>Nếu bạn nghi ngờ hàng giả, hãy liên hệ với chúng tôi bằng số điện thoại cuối trang, cảm ơn!</Span></div>
+								<?php 
+									}
+								?>
 							</div>
 						</div>
 					</div>
 
-					<div class="col-md-4 col-sm-4 col-xs-12">
+					<div class="col-md-4 col-sm-3 col-xs-12">
 						<div class="single_abouts wow slideInRight" data-wow-duration="2s">
 							<img src="images/s.png" alt="" />
 						</div>
@@ -183,27 +207,60 @@
 		<div class="container">
 			<div class="row">
 				<div class="features_content text-center">
+					<h2 style="color:black;">Thành viên</h2>
+					<div class="separetor"></div>
 					<div class="col-md-4">
 						<div class="sinle_features wow slideInUp" data-wow-duration="1s">
 							<img src="images/icon1.png" alt="" />
-							<h5>Turnip greens</h5>
-
+							<h5 style="color:black;"><?php 
+								$curl = curl_init();
+								curl_setopt_array($curl, array(
+									CURLOPT_RETURNTRANSFER => true,
+									CURLOPT_URL => 'http://localhost:3004/api/MilkCompany',
+									CURLOPT_USERAGENT => 'cURL GET Request',
+									CURLOPT_SSL_VERIFYPEER => false
+								));	
+								$resp = curl_exec($curl);
+								$val = json_decode($resp, true);
+								curl_close($curl);
+								echo $val[0]['name'];
+							?></h5>
 						</div>
 					</div>
 					<div class="col-md-4">
 						<div class="sinle_features wow slideInUp" data-wow-duration="1.5s">
-							<img src="images/icon1.png" alt="" />
-							<h5>Turnip greens</h5>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra
-								elementuma. Aliquam erat volutpat. </p>
+							<img src="images/icon2.png" alt="" />
+							<h5 style="color:black;"><?php 
+								$curl = curl_init();
+								curl_setopt_array($curl, array(
+									CURLOPT_RETURNTRANSFER => true,
+									CURLOPT_URL => 'localhost:3004/api/TransportCompany',
+									CURLOPT_USERAGENT => 'cURL GET Request',
+									CURLOPT_SSL_VERIFYPEER => false
+								));	
+								$resp = curl_exec($curl);
+								$val = json_decode($resp, true);
+								curl_close($curl);
+								echo $val[0]['name'];
+							?></h5>
 						</div>
 					</div>
 					<div class="col-md-4">
 						<div class="sinle_features wow slideInUp" data-wow-duration="2s">
-							<img src="images/icon1.png" alt="" />
-							<h5>Turnip greens</h5>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra
-								elementuma. Aliquam erat volutpat. </p>
+							<img src="images/icon3.png" alt="" />
+							<h5 style="color:black;"><?php 
+								$curl = curl_init();
+								curl_setopt_array($curl, array(
+									CURLOPT_RETURNTRANSFER => true,
+									CURLOPT_URL => 'localhost:3004/api/Trader',
+									CURLOPT_USERAGENT => 'cURL GET Request',
+									CURLOPT_SSL_VERIFYPEER => false
+								));	
+								$resp = curl_exec($curl);
+								$val = json_decode($resp, true);
+								curl_close($curl);
+								echo $val[0]['name'];
+							?></h5>
 						</div>
 					</div>
 				</div>
@@ -211,221 +268,54 @@
 		</div>
 	</section><!-- End of Abouts Section -->
 
-	<!-- <section id="special" class="special sections">
-		<div class="container">
-			<div class="row">
-				<div class="head_title text-center wow slideInLeft" data-wow-duration="1.5s">
-					<h2>This month specials</h2>
-					<div class="separetor"></div>
-				</div>
-				<div class="main_special text-center wow slideInUp" data-wow-duration="3s">
-					
-					<div class="col-md-2 col-sm-3 col-xs-6 single_special">
-						<div class="single_special_img">
-							<img src="images/s1.png" alt="" />
-							<div class="single_special_overlay text-center">
-								<h3>Greens fava</h3>
-								<div class="overlay_separetor"></div>
-								<p>Lorem ipsum dolor sit Pellentesque vel enim a</p>
-								<p>17$</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-2 col-sm-3 col-xs-6 single_special">
-						<div class="single_special_img">
-							<img src="images/p2.jpg" alt="" />
-							<div class="single_special_overlay text-center">
-								<h3>Greens fava</h3>
-								<div class="overlay_separetor"></div>
-								<p>Lorem ipsum dolor sit Pellentesque vel enim a</p>
-								<p>17$</p>
-							</div>							
-						</div>
-					</div>
-					<div class="col-md-2 col-sm-3 col-xs-6 single_special">
-						<div class="single_special_img">
-							<img src="images/p3.jpg" alt="" />
-							<div class="single_special_overlay text-center">
-								<h3>Greens fava</h3>
-								<div class="overlay_separetor"></div>
-								<p>Lorem ipsum dolor sit Pellentesque vel enim a</p>
-								<p>17$</p>
-							</div>							
-						</div>
-					</div>
-					<div class="col-md-2 col-sm-3 col-xs-6 single_special">
-						<div class="single_special_img">
-							<img src="images/p4.jpg" alt="" />
-							<div class="single_special_overlay text-center">
-								<h3>Greens fava</h3>
-								<div class="overlay_separetor"></div>
-								<p>Lorem ipsum dolor sit Pellentesque vel enim a</p>
-								<p>17$</p>
-							</div>							
-						</div>
-					</div>
-					<div class="col-md-2 col-sm-3 col-xs-6 single_special">
-						<div class="single_special_img">
-							<img src="images/p5.jpg" alt="" />
-							<div class="single_special_overlay text-center">
-								<h3>Greens fava</h3>
-								<div class="overlay_separetor"></div>
-								<p>Lorem ipsum dolor sit Pellentesque vel enim a</p>
-								<p>17$</p>
-							</div>							
-						</div>
-					</div>
-					<div class="col-md-2 col-sm-3 col-xs-6 single_special">
-						<div class="single_special_img">
-							<img src="images/p6.jpg" alt="" />
-							<div class="single_special_overlay text-center">
-								<h3>Greens fava</h3>
-								<div class="overlay_separetor"></div>
-								<p>Lorem ipsum dolor sit Pellentesque vel enim a</p>
-								<p>17$</p>
-							</div>							
-						</div>
-					</div>
-					<div class="col-md-2 col-sm-3 col-xs-6 single_special">
-						<div class="single_special_img">
-							<img src="images/p6.jpg" alt="" />
-							<div class="single_special_overlay text-center">
-								<h3>Greens fava</h3>
-								<div class="overlay_separetor"></div>
-								<p>Lorem ipsum dolor sit Pellentesque vel enim a</p>
-								<p>17$</p>
-							</div>							
-						</div>
-					</div>
-					<div class="col-md-2 col-sm-3 col-xs-6 single_special">
-						<div class="single_special_img">
-							<img src="images/p5.jpg" alt="" />
-							<div class="single_special_overlay text-center">
-								<h3>Greens fava</h3>
-								<div class="overlay_separetor"></div>
-								<p>Lorem ipsum dolor sit Pellentesque vel enim a</p>
-								<p>17$</p>
-							</div>							
-						</div>
-					</div>
-					<div class="col-md-2 col-sm-3 col-xs-6 single_special">
-						<div class="single_special_img">
-							<img src="images/p4.jpg" alt="" />
-							<div class="single_special_overlay text-center">
-								<h3>Greens fava</h3>
-								<div class="overlay_separetor"></div>
-								<p>Lorem ipsum dolor sit Pellentesque vel enim a</p>
-								<p>17$</p>
-							</div>							
-						</div>
-					</div>
-					<div class="col-md-2 col-sm-3 col-xs-6 single_special">
-						<div class="single_special_img">
-							<img src="images/p3.jpg" alt="" />
-							<div class="single_special_overlay text-center">
-								<h3>Greens fava</h3>
-								<div class="overlay_separetor"></div>
-								<p>Lorem ipsum dolor sit Pellentesque vel enim a</p>
-								<p>17$</p>
-							</div>							
-						</div>
-					</div>
-					<div class="col-md-2 col-sm-3 col-xs-6 single_special">
-						<div class="single_special_img">
-							<img src="images/p2.jpg" alt="" />
-							<div class="single_special_overlay text-center">
-								<h3>Greens fava</h3>
-								<div class="overlay_separetor"></div>
-								<p>Lorem ipsum dolor sit Pellentesque vel enim a</p>
-								<p>17$</p>
-							</div>							
-						</div>
-					</div>
-					<div class="col-md-2 col-sm-3 col-xs-6 single_special">
-						<div class="single_special_img">
-							<img src="images/s1.png" alt="" />
-							<div class="single_special_overlay text-center">
-								<h3>Greens fava</h3>
-								<div class="overlay_separetor"></div>
-								<p>Lorem ipsum dolor sit Pellentesque vel enim a</p>
-								<p>17$</p>
-							</div>							
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section> -->
-
 	<!-- footer Section -->
 	<footer id="footer" class="footer">
 		<div class="container">
 			<div class="row">
+				<div class="col-sm-6 col-xs-10">
+					<iframe
+						src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.504512211595!2d106.65552001378587!3d10.7726179923241!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752ec3c161a3fb%3A0xef77cd47a1cc691e!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBCw6FjaCBraG9hIFRQLkhDTQ!5e0!3m2!1svi!2s!4v1558786452677!5m2!1svi!2s"
+						width="100%" height="300" frameborder="0" style="border:0" allowfullscreen>
+					</iframe>
+				</div>
 
-				<div class="col-sm-5 col-sm-offset-7 col-xs-10 col-xs-offset-2">
+				<div class="col-sm-6 col-xs-10">
 					<div class="contact_area wow slideInLeft" data-wow-duration="2s">
 						<div class="head_title text-center">
-							<h2>Contact</h2>
+							<h2>Về chúng tôi</h2>
 							<div class="separetor"></div>
 						</div>
 
 						<div class="main_contact_content">
 							<div class="row">
+								<?php 
+								$curl = curl_init();
+								curl_setopt_array($curl, array(
+									CURLOPT_RETURNTRANSFER => true,
+									CURLOPT_URL => 'http://localhost:3004/api/MilkCompany',
+									CURLOPT_USERAGENT => 'cURL GET Request',
+									CURLOPT_SSL_VERIFYPEER => false
+								));	
+								$resp = curl_exec($curl);
+								$val = json_decode($resp, true);
+								curl_close($curl);
+							?>
 								<div class="col-sm-6">
 									<div class="single_contact text-left">
-										<h5>Veggie </h5>
-										<span>3428 Magnolia Avenue</span>
-										<span>Hackettstown, NJ 07840</span>
+										<h5>Địa chỉ :</h5>
+										<span><?php echo $val[0]['country'];?></span>
 									</div>
 								</div>
 
 								<div class="col-sm-6">
 									<div class="single_contact text-left">
-										<h5>Reservations</h5>
-										<span>reservations@vegggie.com</span>
-										<span>+48 202-555-0114</span>
+										<h5>Liên hệ :</h5>
+										<span>Phone: &nbsp; <?php echo $val[0]['phone'];?></span>
+										<span>Email: &nbsp; <?php echo $val[0]['email'];?></span>
 									</div>
 								</div>
 							</div>
-
-							<div class="contact_form_area">
-								<h3>Contact form</h3>
-								<form action="#" id="formid">
-									<div class="row">
-										<div class="col-sm-6 col-xs-6">
-											<div class="form-group">
-												<input type="text" class="form-control" name="name"
-													placeholder="first name">
-											</div>
-										</div>
-
-										<div class="col-sm-6 col-xs-6">
-											<div class="form-group">
-												<input type="email" class="form-control" name="email"
-													placeholder="Email">
-											</div>
-										</div>
-									</div>
-
-									<div class="row">
-										<div class="col-sm-12">
-											<div class="form-group">
-												<textarea class="form-control" name="message" rows="3"
-													placeholder="Message"></textarea>
-											</div>
-
-
-										</div>
-
-										<div class="form_btn_area text-center">
-											<a href="" class="btn">Send</a>
-										</div>
-									</div>
-								</form>
-							</div>
-
 						</div>
-
 					</div>
 				</div>
 			</div>
@@ -435,8 +325,7 @@
 				<div class="copyright_text_area">
 					<div class="col-md-6 col-sm-6 col-xs-12">
 						<div class="single_footer text-right wow zoomIn" data-wow-duration="2s">
-							<p>Made with <i class="fa fa-heart"></i> by <a href="http://bootstrapthemes.co">Bootstrap
-									Themes</a>2016. All Rights Reserved</p>
+							<p><i class="fa fa-heart"></i> Made by Võ Nhật Hồng Quang </p>
 						</div>
 					</div>
 					<div class="col-md-6 col-sm-6 col-xs-12">
